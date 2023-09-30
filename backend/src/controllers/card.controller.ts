1065452dd37c8c6aa301f7655840b9e27097c740
Josip Simovic <josip.simovic123@gmail.com>
2023-09-30 15:57:39 +0200

    Added @bind to confirmPayment

--- backend/src/controllers/card.controller.ts
+++ backend/src/controllers/card.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import { CardService } from "../services/card.service";
+import bind from "bind-decorator";
 
 export class CardController {
   constructor(private readonly cardService: CardService) {}
 
+  @bind
   async confirmPayment(req: Request, res: Response, next: NextFunction) {
     const { cnumber, cvv }: { cnumber: number; cvv: number } = req.body;
 
